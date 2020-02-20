2708451a6842e5d4e9a69c1f06780394524f6afa
wikicygan <wikicygan@users.noreply.github.com>
2020-02-20 13:17:52 +0100

    pierwsza wersja gry

---
+++ runner.py
@@ -0,0 +1,26 @@
+import numpy as np
+class Player:
+			def __init__(self, name):
+				self.name = name
+			def choice(self, x)
+
+
+class Board:
+	def __init__(self):
+		matrix = [[0, 1, 2],
+				  [3, 4, 5],
+				  [6, 7, 8]]
+		self.matrix = np.array(matrix)
+class Game:
+	def __init__(self, name, board, player1, player2):
+	self.name = Board
+	self.player1 = player1
+	self.player2 = player2
+
+	while True
+		for i in range(2)
+			if i == 0:
+				round = 0
+			if i == 1
+				round = 1
+		break if
